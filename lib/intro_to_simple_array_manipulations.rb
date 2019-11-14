4e6c5447bbc228387bb45bf9e827feba313d8df0
Benjamin Small <ben20262@aim.com>
2019-11-14 00:04:05 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -27,7 +27,7 @@ def using_concat(array1, array2)
 end
 
 def using_insert(array, element)
-  array.insert(element)[4]
+  array[4].insert(element)
 end
 
 def using_uniq(array)
@@ -43,5 +43,5 @@ def using_delete(array, string)
 end
 
 def using_delete_at(array, interger)
-  array.delete()[interger]
+  array[interger].delete()
 end
